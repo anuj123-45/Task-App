fd44e815774cb07d57f697e23a21f75981273d59
anuj123-45 <anuj1358.be20@chitkara.edu.in>
2022-08-09 09:40:32 +0530

    card dynamically added

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 console.log("Just started scripting");
 
+const taskContainer=document.querySelector(".task_container");
+// console.log(taskContainer);
+
+const newCard=({id,imageUrl,taskTitle,taskType,taskDescription})=>
+    `
+<div class="col-md-6 col-lg-4" id=${id}>
+  <div class="card ">
+    <div class="card-header d-flex justify-content-end gap-2">
+      <button type="button" class="btn btn-outline-success  "><i class="fa-solid fa-pencil"></i></button>
+      <button type="button" class="btn btn-outline-danger  "><i class="fa-solid fa-trash-can"></i></button>
+    </div>
+    <img src="${imageUrl}" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${taskTitle}</h5>
+      <p class="card-text">${taskType}</p>
+      <h5><span class="badge bg-primary">${taskDescription}</span></h5>
+    </div>
+    <div class="card-footer text-muted">
+      <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+    </div>
+ </div>
+</div>
+`;
+
+
 const saveChanges=()=>{
     const taskData={
         id:`${Date.now()}`,    // unique no for card id
@@ -10,5 +35,8 @@ const saveChanges=()=>{
 
 
     };
-    console.log(taskData);
+    const cardData=newCard(taskData);
+     taskContainer.insertAdjacentHTML("beforeend",cardData);
 };
+
+
